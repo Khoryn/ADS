a2488dc32063a828e6ea767482f503420b01abb9
Bernardo <bmckay1996@hotmail.com>
2020-09-25 09:05:18 +0100

    Generic Singleton

---
+++ Generic Singleton/Generic Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton<int> test = Singleton<int>.Instance;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Generic Singleton/Generic Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Singleton
+{
+    public class Singleton <T> 
+    {
+        private static Singleton<T> instance = null;
+
+        public static Singleton<T> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton<T>();
+                }
+                return instance;
+            }
+        }
+    }
+}
