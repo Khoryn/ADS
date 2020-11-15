341f393f4464e15225c5d8e400d96bbed1499cf6
Bernardo <bmckay1996@hotmail.com>
2020-11-15 23:43:50 +0000

    ADS_Tarefa_03

---
+++ ADS_UNIT_TEST/Assets/Editor/PointCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System;
+
+public class PointCalculatorTests
+{
+    #region Calculate Points
+    [Test]
+    [TestCase(3, 0, 1, 300)]
+    [TestCase(5, 0, 1, 500)]
+    [TestCase(5, 0, 2, 1000)]
+    [TestCase(5, 1, 2, 800)]
+    [TestCase(10, 0, 1, 1000 + 500)]
+    [TestCase(21, 0, 2, 6200)]
+    [TestCase(11, 3, 1, 1300)]
+    [TestCase(10, 10, 5, 500)]
+    [TestCase(0, 0, 1, 0)]
+    [TestCase(0, 4, 1, 0)]
+
+    public void CalculatePoints_Test(int killedEnemies, int killedVillagers, int multiplicator, int expectedPoints)
+    {
+        PointCalculator pointCalculator = new PointCalculator();
+
+        int points = pointCalculator.CalculatePoints(killedEnemies, killedVillagers, multiplicator);
+
+        Assert.That(points, Is.EqualTo(expectedPoints));
+    }
+    #endregion
+
+
+    [Test]
+    public void CalculatePoints_NegativeKilledEnemies_ThrowsExceptions()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new PointCalculator().CalculatePoints(-1, 0, 1));
+    }
+
+    [Test]
+    public void CalculatePoints_NegativeKilledVillagers_ThrowsExceptions([Values(-2,-1)] int killedVillagers)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new PointCalculator().CalculatePoints(0, killedVillagers, 1));
+    }
+
+    [Test]
+    public void CalculatePoints_NegativeOrZeroMultiplicator_ThrowsExceptions([Values(0, -1)]int multiplicator)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new PointCalculator().CalculatePoints(0, -1, multiplicator));
+    }
+}
---
+++ ADS_UNIT_TEST/Assets/Scripts/PointCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PointCalculator
+{
+    public int CalculatePoints(int killedEnemies, int killedVillagers, int multiplicator)
+    {
+        if (killedEnemies < 0)
+        {
+            throw new ArgumentOutOfRangeException("Killed enemies must be positive");
+        }
+
+        if (killedVillagers < 0)
+        {
+            throw new ArgumentOutOfRangeException("Killed villagers must be positive");
+        }
+
+        if (multiplicator < 0)
+        {
+            throw new ArgumentOutOfRangeException("Multiplicator must be greater than zero");
+        }
+
+        int points = 0;
+
+        points += killedEnemies * 100;
+        points +=  Mathf.FloorToInt(killedEnemies / 10) * 500;
+        points -= killedVillagers * 100;
+
+        if (killedVillagers >= 10)
+        {
+            multiplicator = 1;
+        }
+
+        points *= multiplicator;
+
+        return Mathf.Max(points,0);
+    }
+}
